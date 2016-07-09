b88f9a30ae05a01b8ad1b3d5bb8777df961028ac
artidotia <artidotia@gmail.com>
2016-07-09 10:34:40 +0530

    Session 1 Assignment 2 First Commit
    Session 1 Assignment 2 First Commit

---
+++ Session1Assign2/src/Session1Assign2.java
@@ -0,0 +1,13 @@
+
+public class Session1Assign2 {
+
+	public static void main(String[] args) {
+		int a, b = 4, c; //warning comes as we have not used the variables anywhere. 
+/*		a = 1;
+		c = 5;
+		System.out.println("Value of b is: "+ b);
+		System.out.println("Value of a is: "+ a);
+		System.out.println("Value of c is: "+ c);
+*/	}
+
+}
